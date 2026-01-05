64e075d5d6946cba56080779cd98f582457cfbe3
artavia17 <artaviaalonso60@gmail.com>
2026-01-05 09:55:23 -0600

    Se agrego la carta

--- src/pages/Awards.tsx
+++ src/pages/Awards.tsx
@@ -11,6 +11,15 @@ const Awards = () => {
                         <p>Descubrí la nueva colección <strong>Chiky & Stranger Things</strong>, una edición limitada inspirada en el misterioso mundo del “Upside Down”. Esta línea exclusiva incluye mochilas, cartucheras y loncheras con <strong>detalles bordados y serigrafía de alta densidad</strong>, que combinan el estilo urbano con la esencia de la icónica serie. Con tonos intensos en <strong>negro y rojo</strong>, cada artículo refleja una fusión perfecta entre aventura, nostalgia y autenticidad.</p>
                         <img src={AwardsMochilas} alt="Mochilas de la colección Chiky & Stranger Things" />
                     </section>
+                    <section className='reclamar'>
+                        <h1>¿CÓMO RECLAMAR TUS PREMIOS?</h1>
+                        <ol>
+                            <li><p>Tené a la mano el empaque ganador.</p></li>
+                            <li><p>Prepará una fotocopia de tu documento de identidad.</p></li>
+                            <li><p>Descargá, imprimí y <a href="/pdf/carta.pdf" target="_blank">llená este formulario</a>.</p></li>
+                            <li><p>Presentate con estos documentos en la dirección indicada.</p></li>
+                        </ol>
+                    </section>
                 </div>
             </main>
         </>
